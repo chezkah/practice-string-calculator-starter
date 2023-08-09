7ad066001758eee4846a6610c2fca1c4d5373bd3
Arianne Chezkah Casquete <ariannechezkah@gmail.com>
2023-08-09 10:03:29 +0800

    (Java-Exam): Modify StringCalculator.java to add numbers from a string

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -2,6 +2,12 @@ package com.tw;
 
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        int sum = 0;
+        for (char number: string.toCharArray()) {
+            if (Character.isDigit(number)) {
+                sum += Character.getNumericValue(number);
+            }
+        }
+        return sum;
     }
 }
